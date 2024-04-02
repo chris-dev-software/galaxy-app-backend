ad483e84e1ad2686c121ed2b369da523980e8cc6
Christopher <chris.dev.software@gmail.com>
2024-04-01 23:53:32 -0500

    Terminamos la conexion a la base de datos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import 'dotenv/config.js'
-
 import { app } from './app.js'
+import { sequelize } from './database/database.js'
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`)
-})
+const main = async () => {
+  try {
+    await sequelize.authenticate()
+    console.log('La conexión se ha establecido exitosamente.')
+
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en http://localhost:${PORT}`)
+    })
+  } catch (error) {
+    console.error('No se puede conectar a la base de datos:', error)
+  }
+}
+
+main()
